45328ea54bc6e2d8131b25a1fbfb6a10e4a61032
chekos <sergio@cimarron.io>
2022-10-22 09:52:37 -0700

    full-versions

--- build-db.sh
+++ build-db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## Golden Gate to Oakland
 git-history file traffic.db goldengate-to-oakland-slim.json \
-    --convert 'json.loads("[" + content.decode("utf-8") + "]")' --namespace 'ggoak' --id id
+    --convert 'json.loads("[" + content.decode("utf-8") + "]")' --namespace 'ggoak' --id id --full-versions
 
 ## Oakland to Golden Gate
 git-history file traffic.db oakland-to-goldengate-slim.json \
-    --convert 'json.loads("[" + content.decode("utf-8") + "]")' --namespace 'oakgg' --id id
+    --convert 'json.loads("[" + content.decode("utf-8") + "]")' --namespace 'oakgg' --id id --full-versions
 
